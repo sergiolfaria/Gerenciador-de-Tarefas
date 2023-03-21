ce1a130885388bf5bd07284df704d445da30fb49
Alyson09 <alysonrap13@gmail.com>
2023-03-21 16:17:32 -0300

    redirecionarTXT

--- ReceberNome.java
+++ ReceberNome.java
@@ -8,14 +8,20 @@ class ReceberNome {
       System.out.print("Qual seu nome?");   
       String nome = usuario.nextLine();
 
-      File arquivo = new File(nome + ".txt");
+      File arquivo = new File("C:\\Users\\META-446\\Documents\\EXTRATO DATACORP\\" + nome + ".txt");
 
       try {
          if (arquivo.createNewFile()) {
             System.out.println("Arquivo criado: " + arquivo.getName());
          } else {
             System.out.println("O arquivo já existe.");
+            Scanner leitor = new Scanner(arquivo);
+            while (leitor.hasNextLine()) {
+               String linha = leitor.nextLine();
+               System.out.println(linha);
          }
+         leitor.close();
+        }
       } catch (IOException e) {
          System.out.println("Ocorreu um erro ao criar o arquivo.");
          e.printStackTrace();
